fcce77586f5a7505fa9012d7774624a0ae68a9de
Nam Luu <namluu253@gmail.com>
2021-05-21 11:26:15 +0700

    fix logical in code

--- verify.py
+++ verify.py
@@ -7,8 +7,9 @@ from src.recognition import recognize_engine
 # add text-to-speech
 tts = pyttsx3.init()
 
+input_embeddings = create_embeddings()
+
 while True:
-    input_embeddings = create_embeddings()
     if input_embeddings != {}:
         print("Face Verification in process")
         # tts.runAndWait()
